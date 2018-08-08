45b78354ccc1a0dcbaad2415867cbe0578185ba6
Diego Ávalos <gasparionni@gmail.com>
2018-08-08 17:38:52 -0500

    Added modal to component

--- src/componentes/container/Registro.js
+++ src/componentes/container/Registro.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import Modal from 'react-modal';
+import ReactModal from 'react-modal';
 
-class Registro extends Component{
+class Registro extends Component {
+    constructor() {
+      super()
+      this.state = {
+        showModal: false
+      }
+
+      this.handleOpenModal = this.handleOpenModal.bind(this);
+      this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    handleOpenModal () {
+      this.setState({ showModal: true });
+    }
+    
+    handleCloseModal () {
+      this.setState({ showModal: false });
+    }
+
     render() {
       return (
-        <button onClick={this.MostrarComponente}>Registro</button>
+        <div>
+          <button onClick={this.handleOpenModal}>Registro</button>
+          <ReactModal isOpen={this.state.showModal} contentLabel="Minimal Modal Example">
+            <button onClick={this.handleCloseModal}>Close Modal</button>
+          </ReactModal>
+        </div>
       )
     }
 }
