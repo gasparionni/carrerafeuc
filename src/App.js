30e9770e4828a8c8db7bca3481c3dc0537fe4e4f
cedeno <ccedeno@ucol.mx>
2018-08-08 20:35:55 -0500

    Commit de todo

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
       <div>
         <CenteredGridHeader/>
         <CenteredGridContainer/>
-        <CenteredGridFooter/>
         <Registro/>
+        <CenteredGridFooter/>
       </div>
     );
   }
--- src/componentes/container/Registro.js
+++ src/componentes/container/Registro.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 
+// const MostrarComponente{
 
+// }
+this.state = {
+  toggle: false
+}
+toggle(MostrarComponente){
+  this.state({toggle:true});
+  console.log("True");
+}
 
 class Registro extends Component{
     render() {
       return (
-        <button onClick={this.MostrarComponente}>Registro</button>
+        <button onClick={this.MostrarComponente}>Registrarme!</button>
       )
     }
 }
