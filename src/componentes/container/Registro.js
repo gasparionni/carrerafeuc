8f79db89ad70f984d880dee2914c6a3235e45304
cedeno <ccedeno@ucol.mx>
2018-08-15 21:20:33 -0500

    Ordenar por componentes para hacerlo responsivo

--- src/componentes/container/Registro.js
+++ src/componentes/container/Registro.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 import './modal.css';
+import './estilosformulario.css';
+
+
 class Registro extends Component {
     constructor() {
       super()
@@ -19,14 +22,12 @@ class Registro extends Component {
 
     render() {
       return (
-        <div>
-          <button onClick={this.handleOpenModal}>Registro</button>
-          <div className="modal">
-            <ReactModal isOpen={this.state.showModal} contentLabel="Minimal Modal Example" className="modalestilos">
-              <Formulario/> 
-              <button onClick={this.handleCloseModal}>Aceptar</button>
-            </ReactModal>
-          </div>
+        <div className="content-modal">
+          <button onClick={this.handleOpenModal} className="btn-registrar"><span>Registro</span></button>
+          <ReactModal isOpen={this.state.showModal} contentLabel="Minimal Modal Example" >
+            <Formulario/> 
+            <button onClick={this.handleCloseModal} className="btn-registrar"><span>Aceptar</span></button>
+          </ReactModal>
         </div>
       )
     }
@@ -55,66 +56,71 @@ class Formulario extends Component {
 
   render() {
     return (
-      <div>
-        {/* apellido paterno */}
-        <label htmlFor="lastname">Apellido Paterno</label>
-        <input 
-        value={this.state.lastname} 
-        onChange={this.onChange.bind(this)} 
-        type="text" name="lastname" id="lastname"/>
+      <div className="container">
+        <div className="caja">
+          {/* apellido paterno */}
+          <label htmlFor="lastname">Apellido Paterno:</label>
+          <input 
+          value={this.state.lastname} 
+          onChange={this.onChange.bind(this)} 
+          type="text" name="lastname" id="lastname"/>
 
-        {/* apellido materno */}
-        <label htmlFor="lastname2">Apellido Paterno</label>
-        <input 
-        value={this.state.lastname2} 
-        onChange={this.onChange.bind(this)} 
-        type="text" name="lastname2" id="lastname2"/>
+          {/* apellido materno */}
+          <label htmlFor="lastname2">Apellido Paterno:</label>
+          <input 
+          value={this.state.lastname2} 
+          onChange={this.onChange.bind(this)} 
+          type="text" name="lastname2" id="lastname2"/>
 
-        {/* nombres */}
-        <label htmlFor="name">Nombre/s</label>
-        <input 
-        value={this.state.name} 
-        onChange={this.onChange.bind(this)} 
-        type="text" name="name" id="name"/>
+          {/* nombres */}
+          <label htmlFor="name">Nombre/s:</label>
+          <input 
+          value={this.state.name} 
+          onChange={this.onChange.bind(this)} 
+          type="text" name="name" id="name"/>
+        </div>
+        <div>
+          {/* genero */}
+          <label htmlFor="sex">Sexo:</label>
+          <select name="sex" id="sex" 
+            value={this.state.sex}
+            onChange={this.onChange.bind(this)}>
+            <option value="N/D" select="true">Selecciona</option>
+            <option value="Masculino">Masculino</option>
+            <option value="Femenino">Femenino</option>
+          </select>
+        </div>
+        <div>
+          {/* categoria a correr */}
+          <label htmlFor="categoria">Categoria:</label>
+          <input type="radio" name="categoria" id="categoria" value="10k"
+            onChange={this.onChange.bind(this)}/>10k
+          <input type="radio" name="categoria" id="categoria" value="5k"
+            onChange={this.onChange.bind(this)}/>5k
+          <input type="radio" name="categoria" id="categoria" value="3k"
+            onChange={this.onChange.bind(this)}/>3k
+        </div>
+        <div>
+          {/* edad */}
+          <label htmlFor="year">Edad:</label>
+          <input type="text" name="year" id="year" 
+            value={this.state.year}
+            onChange={this.onChange.bind(this)}/>
+        </div>
+        <div>
+          {/* Numero de telefono  validar la longitud del numero */}
+          <label htmlFor="phonenumber">Telefono:</label>
+          <input value={this.state.phonenumber}
+            onChange={this.onChange.bind(this)}
+            type="text" name="phonenumber" id="phonenumber"/>
 
-        {/* genero */}
-        <label htmlFor="sex">Sexo</label>
-        <select name="sex" id="sex" 
-          value={this.state.sex}
-          onChange={this.onChange.bind(this)}>
-          <option value="N/D" select="true">Selecciona</option>
-          <option value="Masculino">Masculino</option>
-          <option value="Femenino">Femenino</option>
-        </select>
-
-        {/* categoria a correr */}
-        <label htmlFor="categoria">Categoria</label>
-        <input type="radio" name="categoria" id="categoria" value="10k"
-          onChange={this.onChange.bind(this)}/>10k
-        <input type="radio" name="categoria" id="categoria" value="5k"
-          onChange={this.onChange.bind(this)}/>5k
-        <input type="radio" name="categoria" id="categoria" value="3k"
-          onChange={this.onChange.bind(this)}/>3k
-
-        {/* edad */}
-        <label htmlFor="year">Edad</label>
-        <input type="number" name="year" id="year" 
-          value={this.state.year}
-          onChange={this.onChange.bind(this)}/>
-
-        {/* Numero de telefono  validar la longitud del numero */}
-        <label htmlFor="phonenumber">Telefono</label>
-        <input value={this.state.phonenumber}
-          onChange={this.onChange.bind(this)}
-          type="text" name="phonenumber" id="phonenumber"/>
-
-        {/* correo electronico */}
-        <label htmlFor="email">Correo electronico</label>
-        <input 
-          value={this.state.email} 
-          onChange={this.onChange.bind(this)}
-          type="email" name="email" id="email"/>
-
+          {/* correo electronico */}
+          <label htmlFor="email">Correo electronico:</label>
+          <input 
+            value={this.state.email} 
+            onChange={this.onChange.bind(this)}
+            type="email" name="email" id="email"/>
+        </div>
         {/* datos extras */}
         <label htmlFor="extradata">EN CASO DE PADECER ALGUNA ENFERMEDAD Y/O TENER ALGÚN TIPO DE ALERGIA FAVOR DE ESPECIFICARLO: </label>
         <textarea name="extradata" id="extradata" cols="30" rows="10" 
@@ -123,14 +129,14 @@ class Formulario extends Component {
         {/* ajustar lo grande del textarea y la longitud de caracteres xd */}
 
         {/* nombre del contacto de emg */}
-        <label htmlFor="namecontactemg">Nombre/s</label>
+        <label htmlFor="namecontactemg">Nombre/s:</label>
         <input  
           type="text" name="namecontactemg" id="namecontactemg"
           value={this.state.namecontactemg} 
           onChange={this.onChange.bind(this)}/>
 
         {/* contacto de emergencia,  validar la longitud del numero */}
-        <label htmlFor="contactemg">Telefono</label>
+        <label htmlFor="contactemg">Telefono:</label>
         <input type="text" name="contactemg" 
           id="contactemg"
           value={this.state.contactemg} 
